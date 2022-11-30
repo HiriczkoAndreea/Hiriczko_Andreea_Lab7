3bcdf43b33a421df93d26f8c94ae4770b47e676d
Andreea Hiriczko <andreeea.hiriczko@gmail.com>
2022-11-30 15:27:05 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Hiriczko_Andreea_Lab7;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Hiriczko_Andreea_Lab7;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
